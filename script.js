51bff001da19c1f26f37d090315b4d0aeea9757c
airp0 <103009672+aipr0@users.noreply.github.com>
2023-03-08 08:17:59 +0000

    Create function to call individual math functions, include tests for all

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ function divOp(a, b) {
     return a / b;
 }
 
+function operate(operator, a, b) {
+    if(operator === "add") {
+        return addOp(a, b);
+    } else if(operator === "subtract") {
+        return subOp(a, b);
+    } else if(operator === "multiply") {
+        return multOp(a, b);
+    } else if(operator === "divide") {
+        return divOp(a, b);
+    }
+}
+
+console.log(operate("add", 2, 2)); //Should be 4
+console.log(operate("subtract", 10, 1)); //Should be 9
+console.log(operate("multiply", 5, 5)); //Should be 25
+console.log(operate("divide", 90, 9)); //Should be 10
