605fbaa4f3fa8e3dafd0dcfb35689452875f2c7c
airp0 <103009672+aipr0@users.noreply.github.com>
2023-03-08 06:42:52 +0000

    Create functions for basic math operations

---
+++ script.js
@@ -0,0 +1,16 @@
+function addOp(a, b) {
+    return a + b;
+}
+
+function subOp(a, b) {
+    return a - b;
+}
+
+function multOp(a, b) {
+    return a * b;
+}
+
+function divOp(a, b) {
+    return a / b;
+}
+
