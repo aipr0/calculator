32d99834eb825120f69aa6680c7ef43acbb8029b
airp0 <103009672+aipr0@users.noreply.github.com>
2023-03-29 04:49:33 +0100

    Allow CLR button to reset display and stored values

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ let firstValue = 0;
 let secondValue = 0;
 let operator = "";
 
+// Dummy values for testing first and second numbers used in calculation are reset when CLR is pressed
+// firstValue = 5;
+// secondValue = 2;
+//  console.log(firstValue)
+//  console.log(secondValue)
+
+function resetValues() {
+    firstValue = 0;
+    secondValue = 0;
+    operator = "";
+}
 
 function addOp(a, b) {
     return a + b;
@@ -31,27 +42,33 @@ function operate(operator, a, b) {
     }
 }
 
-function namedCallBack(e) {
-    console.log(e.target.textContent);
+function calculate(e) {
+    let btnInput = e.target.textContent;
+    console.log(btnInput);
+
+    document.querySelector(".display").textContent += btnInput;
+
+    if(btnInput === "CLR") {
+        document.querySelector(".display").textContent = ""
+        resetValues();
+        // console.log(firstValue)
+        // console.log(secondValue)
+    }
+
 }
 
 function getButtonValue() {
     const btns = document.querySelectorAll('button');
 
     btns.forEach(btn => {
-        btn.addEventListener('click', namedCallBack);
+        btn.addEventListener('click', calculate);
     })
-}
-
-
-function calculate() {
-
 }
 
 getButtonValue()
 
-//Testing calculator operations
-console.log(operate("add", 2, 2)); //Should be 4
-console.log(operate("subtract", 10, 1)); //Should be 9
-console.log(operate("multiply", 5, 5)); //Should be 25
-console.log(operate("divide", 90, 9)); //Should be 10
+// Testing calculator operations
+// console.log(operate("add", 2, 2)); //Should be 4
+// console.log(operate("subtract", 10, 1)); //Should be 9
+// console.log(operate("multiply", 5, 5)); //Should be 25
+// console.log(operate("divide", 90, 9)); //Should be 10
