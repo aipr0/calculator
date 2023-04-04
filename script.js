1f84edeec9971ea90ed94e9d625f91e26945e05b
airp0 <103009672+aipr0@users.noreply.github.com>
2023-04-04 12:36:11 +0100

    Allow user to use negative integers in both stages of calculation

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let firstValue = 0;
 let secondValue = 0;
 let operator = "";
 let operatorInUse = false;
+let minusPrefixInUse = false;
 
+//When called by CLR button being pressed, will reset values used in calculations
 function resetValues() {
     firstValue = 0;
     secondValue = 0;
@@ -13,19 +15,17 @@ function resetValues() {
 function addOp(a, b) {
     return a + b;
 }
-
 function subOp(a, b) {
     return a - b;
 }
-
 function multOp(a, b) {
     return a * b;
 }
-
 function divOp(a, b) {
     return a / b;
 }
 
+//Called by "=" button being pressed, uses displayed string values
 function operate(operator, a, b) {
     a = parseInt(a);
     b = parseInt(b);
@@ -41,10 +41,12 @@ function operate(operator, a, b) {
     }
 }
 
+//Gets the text content from the calculator's display
 function getCurrentCalcValue() {
      return document.querySelector(".display-text").textContent;
 }
 
+//Clears calculator display
 function clearCalculator() {
     document.querySelector(".display-text").textContent = "";
 }
@@ -70,18 +72,28 @@ function calculate(e) {
         } else {
             secondValue = getCurrentCalcValue();
 
+            //Carries over the sum to be used as the first value in the next calculation
             firstValue = operate(operator, firstValue, secondValue);
             document.querySelector(".display-text").textContent = firstValue;
+
             operatorInUse = false;
+            minusPrefixInUse = false;
+        }
+    } else if(operatorInUse === true) {
+        if(btnInput === "-" && minusPrefixInUse === false) {
+            document.querySelector(".display-text").textContent += "-";
+            minusPrefixInUse = true;
         }
     } else if(operatorInUse === false) {
         if(getCurrentCalcValue() === "") {
-            if(btnInput === "-") {
+            if(btnInput === "-" && minusPrefixInUse === false) {
                 document.querySelector(".display-text").textContent += "-";
+                minusPrefixInUse = true;
             }
         } else {
             operator = btnInput;
             operatorInUse = true;
+            minusPrefixInUse = false;
 
             firstValue = getCurrentCalcValue();
             document.querySelector(".display-text").textContent = "";
